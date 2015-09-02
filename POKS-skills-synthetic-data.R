5376957103ff73b66aa91c63c21904e63f8a4418
Sameer Bhatnagar <sameer.bhatnagar@gmail.com>
2015-09-02 15:20:49 -0400

    added more proto-students to synthetic skill mastery matrix

--- POKS-skills-synthetic-data.R
+++ POKS-skills-synthetic-data.R
@@ -1,17 +1,27 @@
 rm(list = ls())
+source('packages.R')
+##----generate-synth-data-with-defined-prereq-skills----
 
+#each row in the code below is a skill, and there are 12 items 
 q <- t(matrix(c(1,1,1,0,0,0,0,0,0,0,0,0,
               0,0,0,1,1,1,0,0,0,0,0,0,
               0,0,0,0,0,0,1,1,1,0,0,0,
               0,0,0,0,0,0,0,0,0,1,1,1),
             nrow = 12, ncol = 4))
 
-skill.mast.synth.proto <- matrix(c(1,1,1,0,
+#each row in the code below is a student, and there are 4 skills
+# -these proto students are meant to represent a skill structure where
+#   1->2, 1->3, 3->4
+skill.mast.synth.proto <- t(matrix(c(1,1,1,1,
                                    1,1,0,0,
-                                   1,0,1,1,
-                                   1,0,0,1),
-                                 nrow = 4, ncol = 4)
+                                   1,0,1,0,
+                                   0,0,1,1,
+                                   0,0,0,1,
+                                   0,1,1,1,
+                                   0,1,0,1),
+                                 nrow = 4, ncol = 7))
 
+# take prototypical students and replicate to achieve statistical significance in POKS structure induction
 skill.mast <- do.call(rbind, replicate(10, skill.mast.synth.proto, simplify=FALSE))
 
 data.syn <- skill.mast %*% q
